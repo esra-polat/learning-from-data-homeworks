4e5e725900d3034d27363816c2e416ba4c987d39
esra-polat <esrapolat1293@gmail.com>
2020-11-15 04:07:38 +0300

    generalization error

--- homework-3/generalization-error.py
+++ homework-3/generalization-error.py
@@ -3,12 +3,10 @@ import math
 # Hoeffding Inequality
 def lower_bound(M, epsilon = 0.05):
     return math.ceil(-1 / (2 * epsilon**2) * math.log(0.03 / (2 * M)))
-print("")
+
 def calculator(M):
     print("For M:", M, "\tmin N:", lower_bound(M))
 
 calculator(1)
 calculator(10)
 calculator(100)
-
-print("")
